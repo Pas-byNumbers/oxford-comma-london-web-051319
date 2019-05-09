ed2309398790557c82b05deee45fa0f13ded43ae
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-09 19:33:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+  if array.size == 1
+    return array.join
+    elsif array.size == 2
+    array.join(" and ")
+  else
+     array.push[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
+
 end
