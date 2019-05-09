533f767f09842f66532c9d7c617d14300ce41784
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-09 19:28:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,8 @@ def oxford_comma(array)
     return array.join
     elsif array.size == 2
     array.join(" and ")
-    
+    elsif 2 < array.size
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")
 end
